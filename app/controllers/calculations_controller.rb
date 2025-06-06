3692cc44ed7808216832080446e8cf8dfb4b0f05
plaschinsky <gplaschi@chicagobooth.edu>
2025-06-06 16:40:11 +0000

    forms are viewable

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,22 +1,41 @@
 class CalculationsController < ApplicationController
-def square_root_form
-  @sq_root_new_number = params[:sq_root_new_number].to_f
-  @sq_root = Math.sqrt(@sq_root_new_number)
-  render file: Rails.root.join("views", "square_root"), formats: [:html]
-end
+  def square_form
+    # Just renders the form
+  end
 
-def payment_form
-  @APR_value = params[:APR_value].to_f
-  @years_value = params[:years_value].to_i
-  @principal_value = params[:principal_value].to_f
-  @payment = ((@APR_value / 100 / 12) * @principal_value) / (1 - (1 + (@APR_value / 100 / 12))**(-@years_value * 12))
-  render file: Rails.root.join("views", "payment"), formats: [:html]
-end
+  def square_results
+    @number = params[:sq_new_number].to_f
+    @squared = @number ** 2
+  end
 
-def random_form
-  @user_min = params[:user_min].to_f
-  @user_max = params[:user_max].to_f
-  @random_value = rand(@user_min..@user_max)
-  render file: Rails.root.join("views", "random.erb"), formats: [:html]
-end
+  def square_root_form
+    # Just renders the form
+  end
+
+  def square_root_results
+    @sq_root_new_number = params[:sq_root_new_number].to_f
+    @sq_root = Math.sqrt(@sq_root_new_number)
+  end
+
+  def payment_form
+    # Just renders the form
+  end
+
+  def payment_results
+    apr = params[:APR_value].to_f / 100 / 12
+    periods = params[:years_value].to_i * 12
+    principal = params[:principal_value].to_f
+
+    @payment = (apr * principal) / (1 - (1 + apr)**-periods)
+  end
+
+  def random_form
+    # Just renders the form
+  end
+
+  def random_results
+    @user_min = params[:user_min].to_f
+    @user_max = params[:user_max].to_f
+    @random_value = rand(@user_min..@user_max)
+  end
 end
